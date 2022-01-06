e7810327dac547117c28e7ad14b7c513e83e1fbe
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2022-01-06 11:21:13 -0800

    updated App.test and snapshots for Home and Routes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter } from "react-router-dom";
 
 test("renders without crashing", function() {
   render(<MemoryRouter><App /></MemoryRouter>);
 });
+
